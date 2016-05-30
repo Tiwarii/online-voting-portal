16812123d5745fbfa157dc169e4bdc1822c00162
Rashmi Tiwari <tiwarirashmi042@gmail.com>
2016-05-31 05:27:58 +0545

    Added basic entities

---
+++ src/com/ovp/entities/Campaign.java
@@ -0,0 +1,106 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ovp.entities;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Objects;
+
+/**
+ *
+ * @author Rashmi Tiwari
+ */
+public class Campaign {
+    private String title;
+    private Date startDate;
+    private Date endDate;
+    private List<Contestent> contestentList= new ArrayList();
+
+    public Campaign(String title, Date startDate, Date endDate) {
+        this.title = title;
+        this.startDate = startDate;
+        this.endDate = endDate;
+    }
+
+    public void addContestent(Contestent contestent){
+        contestentList.add(contestent);
+    }
+    
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public List<Contestent> getContestentList() {
+        return contestentList;
+    }
+
+    public void setContestentList(List<Contestent> contestentList) {
+        this.contestentList = contestentList;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 29 * hash + Objects.hashCode(this.title);
+        hash = 29 * hash + Objects.hashCode(this.startDate);
+        hash = 29 * hash + Objects.hashCode(this.endDate);
+        hash = 29 * hash + Objects.hashCode(this.contestentList);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Campaign other = (Campaign) obj;
+        if (!Objects.equals(this.title, other.title)) {
+            return false;
+        }
+        if (!Objects.equals(this.startDate, other.startDate)) {
+            return false;
+        }
+        if (!Objects.equals(this.endDate, other.endDate)) {
+            return false;
+        }
+        if (!Objects.equals(this.contestentList, other.contestentList)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Campaign{" + "title=" + title + ", startDate=" + startDate + ", endDate=" + endDate + ", contestentList=" + contestentList + '}';
+    }
+    
+    
+    
+}
---
+++ src/com/ovp/entities/Contestent.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ovp.entities;
+
+import java.util.List;
+import java.util.Objects;
+
+/**
+ *
+ * @author Rashmi Tiwari
+ */
+public class Contestent {
+    private String name;
+    private String picLocation;
+    private List<String> agendaList;
+    private int votes=0;
+
+    public Contestent(String name, String picLocation, List<String> agendaList) {
+        this.name = name;
+        this.picLocation = picLocation;
+        this.agendaList = agendaList;
+    }
+    
+    public void increment(){
+        votes++;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPicLocation() {
+        return picLocation;
+    }
+
+    public void setPicLocation(String picLocation) {
+        this.picLocation = picLocation;
+    }
+
+    public List<String> getAgendaList() {
+        return agendaList;
+    }
+
+    public void setAgendaList(List<String> agendaList) {
+        this.agendaList = agendaList;
+    }
+
+    public int getVotes() {
+        return votes;
+    }
+   
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 67 * hash + Objects.hashCode(this.name);
+        hash = 67 * hash + Objects.hashCode(this.picLocation);
+        hash = 67 * hash + Objects.hashCode(this.agendaList);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Contestent other = (Contestent) obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        if (!Objects.equals(this.picLocation, other.picLocation)) {
+            return false;
+        }
+        if (!Objects.equals(this.agendaList, other.agendaList)) {
+            return false;
+        }
+        return true;
+    }
+    
+    @Override
+    public String toString() {
+        return "Contestent{" + "name=" + name + ", picLocation=" + picLocation + ", agendaList=" + agendaList + '}';
+    }
+
+}
